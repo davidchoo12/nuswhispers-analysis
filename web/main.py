c3e40d42b1d88a34b165edc0df1c82dab5a1f26d
David Choo <7817101+davidchoo12@users.noreply.github.com>
2021-01-09 11:00:17 +0800

    add average per hour graphs

--- web/main.py
+++ web/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 import numpy as np
 import pandas as pd
@@ -25,6 +25,8 @@ app.add_middleware(
 df = pd.read_csv('data.csv')
 for col in ['created_at', 'updated_at', 'status_updated_at']:
     df[col] = pd.to_datetime(df[col])
+df['duration_to_approve'] = (
+    df['status_updated_at'] - df['created_at']) / 36e11
 df.set_index('created_at', inplace=True)
 
 stopwords = set(STOPWORDS)
@@ -33,17 +35,29 @@ stopwords = set(STOPWORDS)
 @app.get('/api/stats')
 async def stats():
     dff = df.reset_index()
+    duration_to_approve_series = df.groupby(df['status_updated_at'].dt.hour)[
+        'duration_to_approve'].mean(numeric_only=False)
+    avg_likes_per_hour_series = df.groupby(df['status_updated_at'].dt.hour)[
+        'fb_like_count'].mean()
+    avg_comments_per_hour_series = df.groupby(df['status_updated_at'].dt.hour)[
+        'fb_comment_count'].mean()
     return {
         'latest': dff.iloc[dff['confession_id'].argmax()]['confession_id'].item(),
         'latest_fbpid': dff.iloc[dff['confession_id'].argmax()]['fb_post_id'].item(),
         'total_approved': len(dff),
-        'days': (pd.Timestamp.today() - dff['created_at'].max()).days,
+        'years': (pd.Timestamp.today() - dff['created_at'].min()).days / 365,
         'most_liked': dff.iloc[dff['fb_like_count'].argmax()]['confession_id'].item(),
         'most_liked_fbpid': dff.iloc[dff['fb_like_count'].argmax()]['fb_post_id'].item(),
         'most_commented': dff.iloc[dff['fb_comment_count'].argmax()]['confession_id'].item(),
         'most_commented_fbpid': dff.iloc[dff['fb_comment_count'].argmax()]['fb_post_id'].item(),
         'high_likes_count': len(dff[dff['fb_like_count'] > 100]),
         'high_comments_count': len(dff[dff['fb_comment_count'] > 100]),
+        'duration_to_approve_x': duration_to_approve_series.index.values.tolist(),
+        'duration_to_approve_y': duration_to_approve_series.values.tolist(),
+        'avg_likes_per_hour_x': avg_likes_per_hour_series.index.values.tolist(),
+        'avg_likes_per_hour_y': avg_likes_per_hour_series.values.tolist(),
+        'avg_comments_per_hour_x': avg_comments_per_hour_series.index.values.tolist(),
+        'avg_comments_per_hour_y': avg_comments_per_hour_series.values.tolist(),
     }
 
 
@@ -60,6 +74,9 @@ async def freq(q: str):
 @app.get("/api/wordcloud")
 async def wc(q: str):
     filtered = df[df['content'].str.contains(q, case=False)]
+    if len(filtered) > 10000:
+        raise HTTPException(
+            status_code=400, detail="Confessions set too large")
     comment_words = ''
     for val in filtered['content']:
         # split the value
