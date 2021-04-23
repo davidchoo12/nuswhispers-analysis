84fd3fe9eb4babebcbd5638e8c72e08aeff813c9
David Choo <7817101+davidchoo12@users.noreply.github.com>
2021-04-24 04:58:31 +0800

    converter 4 threads

--- scraper/converter.py
+++ scraper/converter.py
@@ -137,7 +137,7 @@ def scrape_post_id_range(start_index, end_index):
             logger.error('exception ', exc_info=1)
 
 
-    with ThreadPoolExecutor(max_workers=2, thread_name_prefix='T') as executor:
+    with ThreadPoolExecutor(max_workers=4, thread_name_prefix='T') as executor:
         for i in range(executor._max_workers):
             executor.submit(scrape, q, ses)
 
