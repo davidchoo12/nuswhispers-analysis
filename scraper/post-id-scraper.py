58c1aeda9efbe15e1ac1508130a050c1881daf08
David Choo <7817101+davidchoo12@users.noreply.github.com>
2021-09-18 20:27:21 +0800

    Impersonate Chrome http headers

--- scraper/post-id-scraper.py
+++ scraper/post-id-scraper.py
@@ -38,11 +38,15 @@ ses = requests.Session()
 base_url = 'https://m.facebook.com'
 user_agent = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-    "Gecko/20100101 Firefox/86.0"
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/93.0.4577.82 Safari/537.36"
 )
 default_headers = {
     'User-Agent': user_agent,
-    'Accept-Language': 'en-US,en;q=0.5'
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Upgrade-Insecure-Requests': 1,
 }
 ses.headers.update(default_headers)
 
