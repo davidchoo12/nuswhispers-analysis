5c6e8a3e9a075ce63c4b17a703d63c6771c455ad
David Choo <7817101+davidchoo12@users.noreply.github.com>
2022-04-11 02:50:41 +0800

    update test script

--- scraper/test.py
+++ scraper/test.py
@@ -1,2 +1,20 @@
-f = open('data.csv', 'w')
-f.write('asdf')
+import glob
+import csv
+
+ids = open('post-ids.csv', 'r').readlines()
+files = glob.glob('data-[0-9]*-[0-9]*.csv')
+f = open(files[0], 'r')
+reader = csv.reader(f)
+
+prev_pid = 0
+for i, row in enumerate(reader):
+  if row[3] != ids[i].rstrip():
+    print('index post id mismatch, index %d actual post id %s expected post id %s'.format(i, row[3], ids[i]))
+    break
+  if i != int(row[0]):
+    print('index not consecutive, actual index %s expected index %d', row[0], i)
+    break
+  if int(row[3]) < prev_pid:
+    print('post ids not sorted, index %d post id %s', i, row[3])
+    break
+  prev_pid = int(row[3])
