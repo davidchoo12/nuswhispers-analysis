cbaa296bf49db2bf6652f13ae54ffa020708df77
David Choo <7817101+davidchoo12@users.noreply.github.com>
2022-12-08 01:56:20 +0800

    add rescrape logic
    this is so that data is kept up to date with the latest post and
    rescrape on recent posts to update metrics.
    idea is to track each post last changed and rescrape existing posts with:
      (now - last changed timestamp < min last changed age) OR
        guarantee first "min last changed age" days will get scrape, will scrape if there's any changes beyond the first min last changed age cos last changed timestamp will get updated
      (last scraped at - post timestamp < min last changed age)
        guarantee first "min last changed age" days will get scrape, will scrape the new posts < min last change age if converter stopped for > min last changed age)
    
    converter.py
    add functions to read all rows data file, read and update
    index-last-changed.csv, rescrape
    move main logic to run(args)
    change scrape_post_id_range to return saved rows
    
    merger.py
    change merger to actually read data files and merge overlapping rows

--- scraper/merger.py
+++ scraper/merger.py
@@ -3,7 +3,8 @@ from datetime import datetime
 import glob
 import io
 import shutil
-from converter import scrape_post_id_range
+import csv
+from pathlib import Path
 
 start_time = datetime.now()
 log_format = '%(relativeCreated)8d %(threadName)4s %(message)s'
@@ -14,7 +15,30 @@ file_handler.setFormatter(logging.Formatter(log_format))
 logger = logging.getLogger()
 logger.addHandler(file_handler)
 
-files = glob.glob('data/data-[0-9]*-[0-9]*.csv')
+data_dir = 'data'
+
+all_rows = []
+def merge_csv_into_all_rows(file):
+    reader = csv.reader(file)
+    for row in reader:
+        [index, *_, scraped_at_str] = row
+        index = int(index)
+        if index < len(all_rows):
+            existing_row = all_rows[index]
+            if scraped_at_str > existing_row[-1]:
+                logger.info('merging row %d', index)
+                all_rows[index] = row
+        elif index == len(all_rows):
+            all_rows.append(row)
+        else:
+            logger.info('unexpected file %s, curr row index %d > expected index %d, skipping file', file.name, index, len(all_rows))
+            return False # don't delete
+    return True # can delete
+
+files = glob.glob(data_dir + '/data-[0-9]*-[0-9]*.csv')
+if len(files) < 2:
+    logger.info('no data files to merge, exiting')
+    exit(0)
 indexes = []
 for file in files:
     try:
@@ -28,48 +52,26 @@ for file in files:
 indexes.sort(key=lambda e: (e[0], -e[1]))
 logger.info('indexes %s', indexes)
 
-buf = io.StringIO()
-
-def append_data_file(start, end):
-    path_to_append = 'data/data-%d-%d.csv' % (start, end)
-    buf.write(open(path_to_append).read())
-    logger.info('appended %d-%d', start, end)
-    # soft remove file (move file to trash bin)
-    shutil.move(path_to_append, '/Users/david.choo/.Trash/')
-
-last_saved_index = 0
-prev_end = indexes[0][0]-1
 for start, end in indexes:
-    # nothing more to merge
-    if start == indexes[0][0] and end == indexes[-1][1]:
-        logger.info('nothing more to merge, exiting')
-        exit(1)
-    if start > end:
-        logger.info('unexpected file with start index %d > end index %d, skipping', start, end)
-        continue
-    # ensure the curr start index > prev end index
-    if start <= prev_end:
-        continue
-    # if there is a gap
-    if prev_end + 1 != start:
-        logger.info('missing %d-%d, scraping to fill gap', prev_end+1, start-1)
-        # try to scrape the gap
-        last_saved_index = scrape_post_id_range(prev_end+1, start)
-        if last_saved_index + 1 != start:
-            # failed to fill gap
-            logger.info('failed to fill gap, stopping merge')
-            last_saved_index = prev_end
-            break
-        else:
-            logger.info('scraped %d-%d', prev_end+1, last_saved_index)
-            append_data_file(prev_end+1, last_saved_index)
-    append_data_file(start, end)
-    last_saved_index = end
-    prev_end = end
+    filebasename = 'data-%d-%d.csv' % (start, end)
+    filename = '%s/data-%d-%d.csv' % (data_dir, start, end)
+    with open(filename) as f:
+        can_delete = merge_csv_into_all_rows(f)
+    if can_delete:
+        # hard remove existing file with same name in trash bin
+        Path('/Users/david.choo/.Trash/' + filebasename).unlink(missing_ok=True)
+        # soft remove file (move file to trash bin)
+        shutil.move(filename, '/Users/david.choo/.Trash/')
 
-with open('data/data-%d-%d.csv' % (indexes[0][0], last_saved_index), 'w') as fd:
+# need to use buffer to force csv writer to print \n instead of \r\n as line ending of each row
+buf = io.StringIO(newline=None)
+writer = csv.writer(buf, delimiter=',')
+writer.writerows(all_rows)
+start_index = all_rows[0][0]
+end_index = all_rows[-1][0]
+with open('%s/data-%s-%s.csv' % (data_dir, start_index, end_index), 'w', newline='', encoding='utf-8') as fd:
     buf.seek(0)
     shutil.copyfileobj(buf, fd)
 
-logger.info('merged %d-%d', indexes[0][0], last_saved_index)
+logger.info('merged %s-%s', start_index, end_index)
 logger.info('time elapsed %s', str(datetime.now() - start_time))
